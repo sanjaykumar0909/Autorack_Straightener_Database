8a2dddbc8772069f3d8332c69ef4adfa0e217f06
sanjaykumar0909 <alpha.sanjayj@gmail.com>
2024-01-04 20:58:24 +0530

    change settings for DRF

--- Autorack/settings.py
+++ Autorack/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'mainapp',
     'apis',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,8 +51,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
-
+REST_FRAMEWORK= {'DEFAULT_PERMISSION_CLASSES':
+    ['rest_framework.permissions.AllowAny']
+}
+CORS_ORIGIN_ALLOW_ALL= True
 ROOT_URLCONF = 'Autorack.urls'
 
 TEMPLATES = [
