196b598c551c464524598a0683ed35a00e45db39
sanjaykumar0909 <99422881+sanjaykumar0909@users.noreply.github.com>
2024-01-19 14:51:10 +0530

    patch update

--- mainapp/views/csv_file_data_fetch.py
+++ mainapp/views/csv_file_data_fetch.py
@@ -1,8 +1,10 @@
 import django.http
 from django.http import JsonResponse
 from ..models import CsvFileData as Cfd
+import json
 
 def index(request: django.http.HttpRequest)-> JsonResponse:
     csv_file_serial_num= int(request.GET["id"])
     db_data= Cfd.objects.get(pk= csv_file_serial_num).bend_data
-    return JsonResponse([db_data], safe= False)
+    result= json.loads(db_data)
+    return JsonResponse([result], safe= False)
